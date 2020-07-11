003c346f0951df2bb8382cd35690368033dac02c
Nawaz Mujawar <nawazgmujawar21@gmail.com>
2020-07-11 13:45:34 +0530

    update app.js

--- app.js
+++ app.js
@@ -17,10 +17,18 @@ app.use((req, res, next) => {
     next()
 
 })
+app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+});
 
-app.get("/", (req, res, next) => {
-
-})
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: error.message,
+    });
+});
 
 app.post("/", (req, res, next) => {
     let URL = req.body.URL
